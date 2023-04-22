5c9efc133a0d0ae20f00cab81363c344e47f0f09
ZhemoedovArtem1999 <tema.zhemoedov1999@gmail.com>
2023-04-23 01:21:05 +0300

    4. написал программу

---
+++ FinalControl/FinalControl/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FinalControl
+{
+    internal class Program
+    {
+        static string[] CreateArray(int length)
+        {
+            string[] array = new string[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите {i} элемент массива - ");
+                array[i] = Console.ReadLine()!;
+            }
+
+            return array;
+        }
+
+        static void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static string[] CreateNewArrayMax3Length(string[] array)
+        {
+            string[] newArray = new string[array.Length];
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    newArray[index] = array[i];
+                    index++;
+                }
+                    
+            }
+            Console.WriteLine(newArray.Length);
+            return newArray;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов в массиве - ");
+            int.TryParse(Console.ReadLine(), out int n);
+            string[] array = CreateArray(n);
+            PrintArray(array);
+            string[] newArray = CreateNewArrayMax3Length(array);
+            PrintArray(newArray);
+
+
+
+        }
+    }
+}
